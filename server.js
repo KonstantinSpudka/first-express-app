bed1f411c0fd01fd30bcbcb1bb0f1f62e1d58fa6
Alexandra <aspudka@gmail.com>
2023-03-06 02:11:42 +0200

    Add all

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
 const http = require('http');
-// const app = require('./app');
+const app = require('./app');
 
 // Визначаємо номер порта зі змінних середовища (якщо задано) або по дефолту 5000
 const PORT = process.env.PORT || 5000;
 
 // Створюємо сервер і призначаємо у якості обробника app
-const httpServer = http.createServer((req, res) =>{
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('result');
-});
+const httpServer = http.createServer(app);
 
 // Запускаємо сервер на прослуховування порту 5000
 httpServer.listen(PORT, () => {
